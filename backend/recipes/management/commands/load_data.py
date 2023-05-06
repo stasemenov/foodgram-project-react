e0f0adee78b202c1a28102b1bbda4ecd440e0d19
Станислав Семёнов <leadwebber@gmail.com>
2023-05-06 21:06:12 +0300

    feat: load_data

--- backend/recipes/management/commands/load_data.py
+++ backend/recipes/management/commands/load_data.py
@@ -1,21 +1,37 @@
-import csv
+from csv import reader
 
 from django.conf import settings
 from django.core.management import BaseCommand
 
 from recipes.models import Ingredient, Tag
 
+DATA_PATH = str(settings.BASE_DIR)[:-7] + 'data/'
+
+TABLES_DICT = {
+    Tag: 'tags.csv',
+    Ingredient: 'ingredients.csv',
+}
+
 
 class Command(BaseCommand):
-    help = 'Загрузка данных из csv-файлов'
+    """Загрузка данных из csv-файлов"""
 
-    def handle(self, *args, **kwargs):
+    def handle(self, *args, **options):
         for model, base in TABLES_DICT.items():
-            with open(
-                f'{settings.BASE_DIR}/static/data/{base}',
-                'r', encoding='utf-8'
-            ) as csv_file:
-                reader = csv.DictReader(csv_file)
-                model.objects.bulk_create(model(**data) for data in reader)
+            with open(f'{DATA_PATH}{base}', 'r', encoding='utf-8') as csv_file:
+                if model == Tag:
+                    for tag, color, slug in reader(csv_file):
+                        Tag.objects.get_or_create(
+                            name=tag,
+                            color=color,
+                            slug=slug
+                        )
+                    print('Теги успешно загружены')
 
-        self.stdout.write(self.style.SUCCESS())
+                else:
+                    for name, unit in reader(csv_file):
+                        Ingredient.objects.get_or_create(
+                            name=name,
+                            measurement_unit=unit
+                        )
+                    print('Игредиенты успешно загружены')
